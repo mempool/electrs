97d8e3fdbcdbf135df6d152ff06865fee2d8e236
Roman Zeyde <me@romanzey.de>
2018-11-15 23:10:37 +0200

    Add testnet benchmarking script

---
+++ testnet-bench.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -x
+time curl http://localhost:3001/address/n2zbkoYEUQfXeRmdzzExGxFbyFBGcRAkVL	# 2
+time curl http://localhost:3001/address/mm41aypj76mpTqdRoT5sQ4xrpXYEsGQQ3j	# 15
+time curl http://localhost:3001/address/mkMTj6YCu8pQTLiM5hs2cfS3S1QbSAmyPG	# 59
+time curl http://localhost:3001/address/msYN6FJfvA3p2XoVzgjZpZV4AbEcwBQEEJ	# 396
+time curl http://localhost:3001/address/2NDhzMt2D9ZxXapbuq567WGeWP7NuDN81cg	# 4.5K
+time curl http://localhost:3001/address/msjBEBAqheDdgqVab1mYAh7RULQFjEB9yS	# 8.9K
+time curl http://localhost:3001/address/mxosQ4CvQR8ipfWdRktyB3u16tauEdamGc	# 13K
+time curl http://localhost:3001/address/n2eMqTT929pb1RDNuqEnxdaLau1rxy3efi  # 20K
+time curl http://localhost:3001/address/mgxVT9fzHwYDsgEGJSZekKgYbAyrBkqdpi	# 129K
+time curl http://localhost:3001/address/2N66DDrmjDCMM3yMSYtAQyAqRtasSkFhbmX # 282K
+time curl http://localhost:3001/address/2N8KqVf2fhoeL6pRWeyKRwJVsf3xL3JqcvF # 625K
+time curl http://localhost:3001/address/2N9FY2fckLiZtdUEWSMYPe5Zrs1baHX7foG # 710K
