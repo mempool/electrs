22a819a078a13c80fea9ebf37cb514f6008623d4
junderw <jonathan.underwood4649@gmail.com>
2023-07-24 21:57:10 -0700

    No longer need retry fn

--- src/util/bincode_util.rs
+++ src/util/bincode_util.rs
@@ -53,30 +53,3 @@ where
         .allow_trailing_bytes()
         .deserialize(bytes)
 }
-
-// For deserializing borked DBs
-// Only use in scripts
-pub fn deserialize_big_retry<'a, T>(bytes: &'a [u8]) -> (Result<T, bincode::Error>, IntEncoding)
-where
-    T: serde::Deserialize<'a>,
-{
-    if let Ok(data) = bincode::options()
-        .with_big_endian()
-        .with_varint_encoding()
-        .with_no_limit()
-        .allow_trailing_bytes()
-        .deserialize(bytes)
-    {
-        (Ok(data), IntEncoding::VarInt)
-    } else {
-        (
-            bincode::options()
-                .with_big_endian()
-                .with_fixint_encoding()
-                .with_no_limit()
-                .allow_trailing_bytes()
-                .deserialize(bytes),
-            IntEncoding::FixInt,
-        )
-    }
-}
