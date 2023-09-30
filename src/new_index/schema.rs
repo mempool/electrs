ab7cb3df119c58015366d86f7199929176e218ef
junderw <jonathan.underwood4649@gmail.com>
2023-09-29 22:54:18 -0700

    Fix: Make error messages clearer

--- src/new_index/schema.rs
+++ src/new_index/schema.rs
@@ -671,7 +671,7 @@ impl ChainQuery {
 
             // abort if the utxo set size excedees the limit at any point in time
             if utxos.len() > limit {
-                bail!(ErrorKind::TooPopular)
+                bail!(ErrorKind::TooManyUtxos(limit))
             }
         }
 
